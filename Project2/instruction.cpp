dfc03be27f597d8e29bed07d75e95f9165d25874
BrianCrosser <bacrosser@sbcglobal.net>
2015-03-04 11:45:28 -0800

    Update

--- Project2/instruction.cpp
+++ Project2/instruction.cpp
@@ -17,9 +17,11 @@ bitset<6> Instruction::opcode()
 bitset<5> Instruction::rs()
 {
     bitset<5> rs_bits;
+	int counter = 0;
     // Access only the 7th bit to the 11th bit from the 32 bit instruction for R-format and I-format
     for (int i = 6; i < 11; i++){
-		rs_bits[i] = bits[i];
+		rs_bits[counter] = bits[i];
+		counter++;
     }
     return rs_bits;
 }
@@ -27,9 +29,11 @@ bitset<5> Instruction::rs()
 bitset<5> Instruction::rt()
 {
     bitset<5> rt_bits;
-    // Access only the 12th bit to the 16th bit from the 32 bit instruction for R-format and I-format
+	int counter = 0;
+	// Access only the 12th bit to the 16th bit from the 32 bit instruction for R-format and I-format
     for (int i = 11; i < 16; i++){
-		rt_bits[i] = bits[i];
+		rt_bits[counter] = bits[i];
+		counter++;
     }
     return rt_bits;
 }
@@ -37,45 +41,55 @@ bitset<5> Instruction::rt()
 bitset<5> Instruction::rd()
 {
     bitset<5> rd_bits;
+	int counter = 0;
     // Access only the 17th bit to the 21nd bit from the 32 bit instruction for R-format
     for (int i = 16; i < 21; i++){
-		rd_bits[i] = bits[i];
-    }
+		rd_bits[counter] = bits[i];
+		counter++;
+	}
     return rd_bits;
 }
 bitset<5> Instruction::shamt()
 {
     bitset<5> shamt_bits;
-    // Access only the 22th bit to the 26nd bit from the 32 bit instruction for R-format
+	int counter = 0;
+	// Access only the 22th bit to the 26nd bit from the 32 bit instruction for R-format
     for (int i = 21; i < 26; i++){
-		shamt_bits[i] = bits[i];
+		shamt_bits[counter] = bits[i];
+		counter++;
     }
     return shamt_bits;
 }
 bitset<6> Instruction::funct()
 {
     bitset<6> funct_bits;
+	int counter = 0;
     // Access only the 27th bit to the last bit from the 32 bit instruction for R-format
     for (int i = 26; i < 32; i++){
-		funct_bits[i] = bits[i];
+		funct_bits[counter] = bits[i];
+		counter++;
     }
     return funct_bits;
 }
 bitset<16> Instruction::imm()
 {
     bitset<16> imm_bits;
-    // Access only the 17th bit to the last bit from the 32 bit instruction for I-format
+	int counter = 0;
+	// Access only the 17th bit to the last bit from the 32 bit instruction for I-format
     for (int i = 16; i < 32; i++){
-		imm_bits[i] = bits[i];
+		imm_bits[counter] = bits[i];
+		counter++;
     }
     return imm_bits;
 }
 bitset<26> Instruction::jmp_offset()
 {
     bitset<26> jmp_offset_bits;
-    // Access only the 7th bit to the last bit from the 32 bit instruction for J-format
+	int counter = 0;
+	// Access only the 7th bit to the last bit from the 32 bit instruction for J-format
     for (int i = 6; i < 32; i++){
-		jmp_offset_bits[i] = bits[i];
+		jmp_offset_bits[counter] = bits[i];
+		counter++;
     }
     return jmp_offset_bits;
 }

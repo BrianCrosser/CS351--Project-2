872bb267d12d475f01a68d4de8fdc87db47d2b5d
BrianCrosser <bacrosser@sbcglobal.net>
2015-03-03 16:42:50 -0800

    All Files Needed now Exist. We need to fill in the functions in instruction.cpp

---
+++ Project2/instruction.cpp
@@ -0,0 +1,41 @@
+#include <bitset>
+#include <iostream>
+#include "instruction.hpp"
+using namespace std;
+
+// Access the different fields
+bitset<6> opcode()
+{
+
+}
+
+bitset<5> rs()
+{
+
+}
+
+bitset<5> rt()
+{
+
+}
+
+bitset<5> rd()
+{
+
+}
+bitset<5> shamt()
+{
+
+}
+bitset<6> funct()
+{
+
+}
+bitset<16> imm()
+{
+
+}
+bitset<26> jmp_offset()
+{
+
+}

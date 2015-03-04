2d06fbec0050ac938dda5eb73b7ce7efe5e9ee50
BrianCrosser <bacrosser@sbcglobal.net>
2015-03-04 10:57:29 -0800

    Junk file. DONT USE

--- Project2/instruction.cpp
+++ Project2/instruction.cpp
@@ -6,36 +6,49 @@ using namespace std;
 // Access the different fields
 bitset<6> opcode()
 {
-
+	for (int i = 0; i < 6; i++)
+	{
+		
+	}
+	return;
 }
 
 bitset<5> rs()
 {
-
+	return;
 }
 
 bitset<5> rt()
 {
-
+	return;
 }
 
 bitset<5> rd()
 {
-
+	return;
 }
+
 bitset<5> shamt()
 {
-
+	return;
 }
+
 bitset<6> funct()
 {
-
+	return;
 }
+
 bitset<16> imm()
 {
-
+	return;
 }
+
 bitset<26> jmp_offset()
 {
+	return;
+}
 
+string to_string()
+{
+	return;
 }

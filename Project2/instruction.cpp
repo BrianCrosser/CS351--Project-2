7a6fcb1c9579601f8f4ab7d5c7dd16c644e6f24b
swinford <swinford@seawolf.sonoma.edu>
2015-03-03 18:41:15 -0800

    Update instruction.cpp

--- Project2/instruction.cpp
+++ Project2/instruction.cpp
@@ -8,7 +8,7 @@ bitset<6> opcode()
 {
     bitset<6> op_bits;
     // Access only the first 6 bits from the 32 bit instruction
-    for (int i = 0; i < 5; i++){
+    for (int i = 0; i < 6; i++){
         op_bits += bits[i];
     }
     return op_bits;
@@ -18,7 +18,7 @@ bitset<5> rs()
 {
     bitset<5> rs_bits;
     // Access only the 7th bit to the 11th bit from the 32 bit instruction for R-format and I-format
-    for (int i = 7; i < 12; i++){
+    for (int i = 6; i < 11; i++){
         rs_bits += bits[i];
     }
     return rs_bits;
@@ -28,7 +28,7 @@ bitset<5> rt()
 {
     bitset<5> rt_bits;
     // Access only the 12th bit to the 16th bit from the 32 bit instruction for R-format and I-format
-    for (int i = 12; i < 17; i++){
+    for (int i = 11; i < 16; i++){
         rt_bits += bits[i];
     }
     return rt_bits;
@@ -38,7 +38,7 @@ bitset<5> rd()
 {
     bitset<5> rd_bits;
     // Access only the 17th bit to the 21nd bit from the 32 bit instruction for R-format
-    for (int i = 17; i < 22; i++){
+    for (int i = 16; i < 21; i++){
         rd_bits += bits[i];
     }
     return rd_bits;
@@ -47,7 +47,7 @@ bitset<5> shamt()
 {
     bitset<5> shamt_bits;
     // Access only the 22th bit to the 26nd bit from the 32 bit instruction for R-format
-    for (int i = 22; i < 27; i++){
+    for (int i = 21; i < 26; i++){
         shamt_bits += bits[i];
     }
     return shamt_bits;
@@ -56,7 +56,7 @@ bitset<6> funct()
 {
     bitset<6> funct_bits;
     // Access only the 27th bit to the last bit from the 32 bit instruction for R-format
-    for (int i = 27; i < 33; i++){
+    for (int i = 26; i < 32; i++){
         funct_bits += bits[i];
     }
     return funct_bits;
@@ -65,7 +65,7 @@ bitset<16> imm()
 {
     bitset<16> imm_bits;
     // Access only the 17th bit to the last bit from the 32 bit instruction for I-format
-    for (int i = 17; i < 33; i++){
+    for (int i = 16; i < 32; i++){
         imm_bits += bits[i];
     }
     return imm_bits;
@@ -74,7 +74,7 @@ bitset<26> jmp_offset()
 {
     bitset<26> jmp_offset_bits;
     // Access only the 7th bit to the last bit from the 32 bit instruction for J-format
-    for (int i = 7; i < 33; i++){
+    for (int i = 6; i < 32; i++){
         jmp_offset_bits += bits[i];
     }
     return jmp_offset_bits;
